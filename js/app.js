81e50b2b949f4abc225fa783f293a8c66e32a1cb
sarahemilym <sarahemendham@gmail.com>
2017-03-27 11:04:50 +0100

    displaying all data for index page

--- js/app.js
+++ js/app.js
@@ -21,13 +21,56 @@ function setUpPage(data){
   var text = document.createTextNode('My YouTube Playlist');
   title.appendChild(text);
   main.appendChild(title);
-  displayThumbnail(main, data);
+  displayTile(main, data);
 }
 
-function displayThumbnail(main, data){
-  data.forEach(function(){
+function displayTile(main, data){
+  data.forEach(function(data){
     var div = document.createElement('div');
     div.classList.add('index');
     main.appendChild(div);
+    addTitle(data, div);
+
   });
+}
+
+function addTitle(data, div){
+  var title = document.createElement('h1');
+  title.classList.add('title');
+  var text = document.createTextNode(data.snippet.title);
+  title.appendChild(text);
+  div.appendChild(title);
+  addPublished(data, div);
+}
+
+function addPublished(data, div){
+  var published = document.createElement('p');
+  published.classList.add('published');
+  var text = document.createTextNode(data.contentDetails.videoPublishedAt);
+  published.appendChild(text);
+  div.appendChild(published);
+  addDescription(data, div);
+}
+
+function addDescription(data, div){
+  var description = document.createElement('p');
+  description.classList.add('description');
+  var text = document.createTextNode(data.snippet.description);
+  description.appendChild(text);
+  div.appendChild(description);
+  checkText(description);
+  addThumbnail(data, div);
+}
+
+function checkText(description){
+  console.log(description.innerHTML.length);
+  if(description.innerHTML.length >157) {
+    description.innerHTML = description.innerHTML.substring(0,157)+'...';
+  }
+}
+
+function addThumbnail(data, div){
+  var img = document.createElement('img');
+  img.src = data.snippet.thumbnails.default.url;
+  div.appendChild(img);
 }
