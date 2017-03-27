d3c243f8e47ad2b3fb6561560b1884bb1f3cb34e
sarahemilym <sarahemendham@gmail.com>
2017-03-27 12:08:22 +0100

    working of formatting date, result of the data for index and show page is there

--- js/app.js
+++ js/app.js
@@ -16,6 +16,7 @@ function getData(){
 }
 
 function setUpPage(data){
+  console.log(data);
   var main = document.getElementById('main');
   var title = document.createElement('h1');
   var text = document.createTextNode('My YouTube Playlist');
@@ -40,7 +41,58 @@ function addTitle(data, div){
   var text = document.createTextNode(data.snippet.title);
   title.appendChild(text);
   div.appendChild(title);
+  addListener(title, data);
+  // setDate(data, div);
   addPublished(data, div);
+}
+
+// function setDate(data, div){
+//   var num = document.createTextNode(data.snippet.publishedAt);
+//   // console.log(num)
+//   var numS = num.split(0,3);
+//   // var month = num.getMonth();
+//   console.log(numS);
+//   // var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+//   // var splitDate = date.indexOf('T');
+//   // var newDate = num.substring(0, 9);
+//   // var newDate = date.substr(0,9);
+//   // console.log(newDate)
+//   // var newDate = num.substr(num.indexOf('T'));
+// }
+
+function addListener(title, data){
+  title.onclick = function(){
+    var main = document.getElementById('main');
+    main.innerHTML = '';
+    openShowPage(main, data);
+  };
+}
+
+function openShowPage(main, data){
+  backToButton(main);
+  var div = document.createElement('div');
+  div.classList.add('show');
+  main.appendChild(div);
+  addTitle(data, div);
+}
+
+function addVideo(data, div){
+  var videoId = data.contentDetails.videoId;
+  var iframe = document.createElement('iframe');
+  iframe.classList.add('video');
+  iframe.src = 'https://www.youtube.com/embed/' + videoId;
+  div.appendChild(iframe);
+  addDescription(data, div);
+}
+
+function backToButton(main){
+  var button = document.createElement('button');
+  button.innerHTML = '< Back to list of videos';
+  main.appendChild(button);
+  button.onclick = function() {
+    main.innerHTML = '';
+    getData();
+  };
 }
 
 function addPublished(data, div){
@@ -49,7 +101,11 @@ function addPublished(data, div){
   var text = document.createTextNode(data.contentDetails.videoPublishedAt);
   published.appendChild(text);
   div.appendChild(published);
-  addDescription(data, div);
+  if (div.classList.contains('show')){
+    addVideo(data, div);
+  } else {
+    addDescription(data, div);
+  }
 }
 
 function addDescription(data, div){
@@ -59,11 +115,12 @@ function addDescription(data, div){
   description.appendChild(text);
   div.appendChild(description);
   checkText(description);
-  addThumbnail(data, div);
+  if (div.classList.contains('index')){
+    addThumbnail(data, div);
+  }
 }
 
 function checkText(description){
-  console.log(description.innerHTML.length);
   if(description.innerHTML.length >157) {
     description.innerHTML = description.innerHTML.substring(0,157)+'...';
   }
