441d4739c4c67fceb97b1c34f38b3e7309f5588e
sarahemilym <sarahemendham@gmail.com>
2017-03-30 16:42:01 +0100

    added gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,23 @@ const cleanCSS 	  = require('gulp-clean-css');
 const uglify      = require('gulp-uglify');
 const browserSync = require('browser-sync').create();
 
+gulp.task('copy', function () {
+  gulp
+  .src('src/*.html')
+  .pipe(gulp.dest('public'));
+});
+
+gulp.task('copy', function () {
+  gulp
+  .src('src/images/**.**')
+  .pipe(gulp.dest('public/images'));
+});
+
 gulp.task('es6', () => {
   return gulp.src('src/js/*.js')
   .pipe(babel({ presets: ['es2015'] }))
   .pipe(uglify())
   .pipe(gulp.dest('public/js'));
-});
-
-gulp.task('copy', function () {
-  gulp
-  .src('src/*.html')
-  .pipe(gulp.dest('public'));
 });
 
 gulp.task('sass', () => {
@@ -33,9 +39,9 @@ gulp.task('serve', ['es6', 'sass'], () => {
   });
 });
 
-gulp.task('default', ['sass', 'serve', 'copy'], () => {
+gulp.task('default', ['sass', 'serve', 'copy', 'copy'], () => {
   gulp.watch('src/scss/**/*.scss', ['sass']);
   gulp.watch('src/js/*.js', ['es6']);
-  gulp.watch('src/*.html', browserSync.reload);
+  gulp.watch('src/*.html').on('change', browserSync.reload);
   gulp.watch('public/css/*.css').on('change', browserSync.reload);
 });
